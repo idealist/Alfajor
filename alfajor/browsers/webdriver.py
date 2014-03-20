42921a6996509e7e4af7a2689fcc7128bea53ba7
Eric O'Connell <eric.oconnell@idealist.org>
2014-03-19 17:57:16 -0700

    squelch noisy http logs

--- alfajor/browsers/webdriver.py
+++ alfajor/browsers/webdriver.py
@@ -45,7 +45,7 @@ import logging
 # you will see the REQUEST, including HEADERS and DATA, and RESPONSE with HEADERS but without DATA.
 # the only thing missing will be the response.body which is not logged.
 import httplib
-httplib.HTTPConnection.debuglevel = 1
+# httplib.HTTPConnection.debuglevel = 1
 
 logging.basicConfig() # you need to initialize logging, otherwise you will not see anything from requests
 logging.getLogger().setLevel(logging.DEBUG)
